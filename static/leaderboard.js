45caeea8e467784cab750d0b7138cc01f89fa271
camgsab99 <electriqulgaming@gmail.com>
2023-05-02 18:00:49 -0400

    Leaderboard

---
+++ static/leaderboard.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrfQJbCZ5LvcKDKbf_E1YIdSQeDFK4emY",
+  authDomain: "snake-a08fa.firebaseapp.com",
+  projectId: "snake-a08fa",
+  storageBucket: "snake-a08fa.appspot.com",
+  messagingSenderId: "589455344205",
+  appId: "1:589455344205:web:9f50e60ea419f9a6861204"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

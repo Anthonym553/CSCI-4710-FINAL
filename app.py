719963c22c65fa95be07a80eb4a4e2092c98a89a
camgsab99 <electriqulgaming@gmail.com>
2023-05-02 14:22:47 -0400

    Flask Implementation

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/game")
+@app.route("/templates/snake.html")
 def game():
     return render_template("snake.html")
+
+@app.route("/templates/leaderboard.html")
+def leaderboard():
+    return render_template("leaderboard.html")
 
 if __name__ == "__main__":
   app.run()
